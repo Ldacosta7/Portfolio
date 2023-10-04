b9858a4f981401e45b8f9559ea8d561552da40fc
Luco7 <Ldacosta7797@gmail.com>
2023-10-04 19:08:04 +0200

    modification de texte au centre de la video

---
+++ app.js
@@ -0,0 +1,5 @@
+// Utilisation de JavaScript pour activer l'animation
+window.addEventListener('load', function () {
+    var textElement = document.querySelector('.animate-text');
+    textElement.style.opacity = '1'; // Rend le texte visible
+});
